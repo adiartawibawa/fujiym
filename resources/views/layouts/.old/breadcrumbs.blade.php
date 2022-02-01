8f5f38184850d5e9545da3d3106d6c996fdc347c
adiartawibawa <surat.buat.adi@gmail.com>
2022-02-01 12:12:08 +0800

    delete old layouts

--- resources/views/layouts/.old/breadcrumbs.blade.php
+++
@@ -1,8 +0,0 @@
-<ul data-v-69efce2b="">
-    <li class="title-stack-item inline-block pr-1 text-gray-500 last:pr-0 last:font-black last:text-black dark:text-gray-100"
-        data-v-69efce2b="">Admin</li>
-    <li class="title-stack-item inline-block pr-1 text-gray-500 last:pr-0 last:font-black last:text-black dark:text-gray-100"
-        data-v-69efce2b=""> / </li>
-    <li class="title-stack-item inline-block pr-1 text-gray-500 last:pr-0 last:font-black last:text-black dark:text-gray-100"
-        data-v-69efce2b="">Dashboard</li>
-</ul>
